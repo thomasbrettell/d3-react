6f3ca77d5f58abbaeec12f20a1ba51961f16335f
thomasbrettell <thomas.a.brettell@gmail.com>
2021-12-15 22:07:05 +1100

    Add button to navigate to home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { Routes, Route, Navigate, Link } from 'react-router-dom';
 import Home from './Home';
 import Vis1 from './vis/Vis1';
 import Vis2 from './vis/Vis2';
@@ -18,30 +19,39 @@ import MissingMigrants2 from './vis/MissingMigrants/Vis2';
 import MissingMigrants3 from './vis/MissingMigrants/Vis3';
 import Vis12 from './vis/Vis12';
 
+const HomeLink = styled(Link)`
+  position: absolute;
+  right: 0;
+  top: 0;
+`;
+
 const App = () => {
   document.getElementById('fcc_test_suite_wrapper').style.display = 'none';
 
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/vis-1' element={<Vis1 />} />
-      <Route path='/vis-2' element={<Vis2 />} />
-      <Route path='/vis-3' element={<Vis3 />} />
-      <Route path='/vis-4' element={<Vis4 />} />
-      <Route path='/vis-5' element={<Vis5 />} />
-      <Route path='/vis-6' element={<Vis6 />} />
-      <Route path='/vis-7' element={<Vis7 />} />
-      <Route path='/vis-8' element={<Vis8 />} />
-      <Route path='/vis-9' element={<Vis9 />} />
-      <Route path='/vis-10' element={<Vis10 />} />
-      <Route path='/vis-11' element={<Vis11 />} />
-      <Route path='/vis-12' element={<Vis12 />} />
-      <Route path='/missing-migrants/' element={<MissingMigrants />} />
-      <Route path='/missing-migrants/vis-1' element={<MissingMigrants1 />} />
-      <Route path='/missing-migrants/vis-2' element={<MissingMigrants2 />} />
-      <Route path='/missing-migrants/vis-3' element={<MissingMigrants3 />} />
-      <Route path='*' element={<Navigate replace to='/' />} />
-    </Routes>
+    <>
+      <HomeLink to='/'>Home</HomeLink>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/vis-1' element={<Vis1 />} />
+        <Route path='/vis-2' element={<Vis2 />} />
+        <Route path='/vis-3' element={<Vis3 />} />
+        <Route path='/vis-4' element={<Vis4 />} />
+        <Route path='/vis-5' element={<Vis5 />} />
+        <Route path='/vis-6' element={<Vis6 />} />
+        <Route path='/vis-7' element={<Vis7 />} />
+        <Route path='/vis-8' element={<Vis8 />} />
+        <Route path='/vis-9' element={<Vis9 />} />
+        <Route path='/vis-10' element={<Vis10 />} />
+        <Route path='/vis-11' element={<Vis11 />} />
+        <Route path='/vis-12' element={<Vis12 />} />
+        <Route path='/missing-migrants/' element={<MissingMigrants />} />
+        <Route path='/missing-migrants/vis-1' element={<MissingMigrants1 />} />
+        <Route path='/missing-migrants/vis-2' element={<MissingMigrants2 />} />
+        <Route path='/missing-migrants/vis-3' element={<MissingMigrants3 />} />
+        <Route path='*' element={<Navigate replace to='/' />} />
+      </Routes>
+    </>
   );
 };
 
