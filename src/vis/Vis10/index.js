cdcf7685b1b46fd5c0e28ff91f5367a378df6cd4
thomasbrettell <thomas.a.brettell@gmail.com>
2021-12-18 19:48:51 +1100

    add: clock hands to clock

--- src/vis/Vis10/index.js
+++ src/vis/Vis10/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { arc } from 'd3-shape';
 import { pie } from 'd3-shape';
 import VisBox from '../../components/VisBox';
@@ -10,6 +10,11 @@ const Vis = styled.div`
   background-color: white;
   margin-top: 60px;
   display: flex;
+`;
+
+const TimeHand = styled.line`
+  stroke: white;
+  stroke-width: 2;
 `;
 
 const Wedge = styled.g`
@@ -37,6 +42,7 @@ const Wedge = styled.g`
 `;
 
 const Vis10 = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
   const time = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
   const data = time.concat(time);
   const width = 500;
@@ -51,31 +57,74 @@ const Vis10 = () => {
     .innerRadius(0 * 0.2 + (width / 3) * 0.8)
     .outerRadius(0 * 0.2 + (width / 3) * 0.8);
 
+  const hours = currentTime.getHours();
+  const minutes = currentTime.getMinutes();
+  const seconds = currentTime.getSeconds();
+
+  console.log(hours, minutes, seconds);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <VisBox>
       <Vis>
         <svg width={width} height={height}>
+          {useMemo(
+            () => (
+              <g transform={`translate(${centerX}, ${centerY})`}>
+                {pie()
+                  .value(1)(data)
+                  .map((el, i) => (
+                    <Wedge
+                      key={`${el.data}-${i}`}
+                      am={i < data.length / 2}
+                      pm={i >= data.length / 2}
+                    >
+                      <path d={pieArc(el)} data-time={el.data}></path>
+                      <text transform={`translate(${arcLabel.centroid(el)})`}>
+                        {`${el.data}${i < data.length / 2 ? 'am' : 'pm'}`}
+                      </text>
+                      <text
+                        fontSize={'.75rem'}
+                        transform={`translate(${arcLabelSecondary.centroid(
+                          el
+                        )})`}
+                      >
+                        {i}
+                      </text>
+                    </Wedge>
+                  ))}
+              </g>
+            ),
+            [arcLabel, arcLabelSecondary, centerY, centerX, data, pieArc]
+          )}
+
           <g transform={`translate(${centerX}, ${centerY})`}>
-            {pie()
-              .value(1)(data)
-              .map((el, i) => (
-                <Wedge
-                  key={`${el.data}-${i}`}
-                  am={i < data.length / 2}
-                  pm={i >= data.length / 2}
-                >
-                  <path d={pieArc(el)} data-time={el.data}></path>
-                  <text transform={`translate(${arcLabel.centroid(el)})`}>
-                    {`${el.data}${i < data.length / 2 ? 'am' : 'pm'}`}
-                  </text>
-                  <text
-                    fontSize={'.75rem'}
-                    transform={`translate(${arcLabelSecondary.centroid(el)})`}
-                  >
-                    {i}
-                  </text>
-                </Wedge>
-              ))}
+            <TimeHand
+              y2={-(centerY / 2)}
+              y1='38'
+              transform={`rotate(${14 * hours + minutes / 2 + 7.5})`}
+            />
+
+            {/* <TimeHand
+              y2={-(centerY / 0.5)}
+              y1='38'
+              transform={`rotate(${6 * minutes + seconds / 10})`}
+            /> */}
+
+            <TimeHand
+              y2={-(centerY - 50)}
+              y1='38'
+              transform={`rotate(${6 * seconds})`}
+            />
           </g>
         </svg>
       </Vis>
